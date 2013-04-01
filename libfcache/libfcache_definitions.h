2990832e5a966f3b8b6064d069332329b5c8ce86
Joachim Metz <joachim.metz@gmail.com>
2013-04-01 16:23:51 +0200

    Changes of version: 20130401

--- libfcache/libfcache_definitions.h
+++ libfcache/libfcache_definitions.h
@@ -33,11 +33,11 @@
  * for local use of libfcache
  */
 #else
-#define LIBFCACHE_VERSION					20130120
+#define LIBFCACHE_VERSION					20130401
 
 /* The libfcache version string
  */
-#define LIBFCACHE_VERSION_STRING				"20130120"
+#define LIBFCACHE_VERSION_STRING				"20130401"
 
 /* The cache value flags definitions
  */
