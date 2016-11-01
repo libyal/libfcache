94cff6100f6af8e1e218e42441f6e604215664b5
Joachim Metz <joachim.metz@gmail.com>
2016-11-01 18:53:25 +0100

    Worked on tests

--- tests/test_api_functions.ps1
+++ tests/test_api_functions.ps1
@@ -10,7 +10,7 @@ $TestPrefix = Split-Path -path ${Pwd}.Path -parent
 $TestPrefix = Split-Path -path ${TestPrefix} -leaf
 $TestPrefix = ${TestPrefix}.Substring(3)
 
-$TestFunctions = "get_version error date_time" -split " "
+$TestFunctions = "date_time error support" -split " "
 
 $TestToolDirectory = "..\vs2010\Release"
 
